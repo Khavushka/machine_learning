c4a990c560def0931f180e3e0d3e171646d5e97c
Khavushka <khavushka@gmail.com>
2023-04-14 22:51:21 +0200

    Lost commit

--- main.py
+++ main.py
@@ -34,4 +34,21 @@ joblib.dump(model, 'music-recommender.joblib')
 
 # predictions = model.predict([21, 1])
 
+# -------------------------------------
 # visualizing decision trees
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+X = music_data.drop(columns=['genre'])
+y = music_data['genre']
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+tree.export_graphviz(model, out_file='music_recommender.dot', 
+                    feature_names=['age', 'gender'], 
+                    class_names=sorted(y.unique())
+                    label='all', 
+                    rounded=True,
+                    filled=True)
