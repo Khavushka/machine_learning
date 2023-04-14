4985243471d09d4aa16153b1d0fc342f7c1c58b7
Khavushka <khavushka@gmail.com>
2023-04-14 23:12:24 +0200

    Skil af i forskellige colons

---
+++ meach_le.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+'''
+Dataset
+'''
+
+cols = ["first", "second", "third"]
+df = pd.read_csv('spiral.txt', names=cols,  sep='\t')
+df.head()
+
+print(df.head())
