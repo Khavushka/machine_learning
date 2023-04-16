0f049bd3c6021da250ea3d68fde6a53bd1167418
Khavushka <khavushka@gmail.com>
2023-04-16 22:30:41 +0200

    plotting all

--- meach_le.py
+++ meach_le.py
@@ -14,3 +14,13 @@ print(df.head())
 
 # til at convertere, hvis man har g og h til at være 1 0g 0
 # df['first'] = (df['first'] == "g").astype(int) 
+
+
+for label in cols[:-1]:
+    plt.hist(df[df["third"]==1][label], color="blue", label="gamma", alpha = 0.7, density=True)
+    plt.hist(df[df["third"]==0][label], color="red", label="gamma", alpha = 0.7, density=True)
+    plt.title(label)
+    plt.ylabel("Prob")
+    plt.xlabel(label)
+    plt.legend()
+    plt.show()
